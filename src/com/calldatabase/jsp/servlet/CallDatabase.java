49b41a2e516b711400083786cd82126f5d2fbff2
walshtt <walshtt@mail.uc.edu>
2017-06-29 16:26:16 -0400

    add
    example for add event. not 100% positive it works yet

--- src/com/calldatabase/jsp/servlet/CallDatabase.java
+++ src/com/calldatabase/jsp/servlet/CallDatabase.java
@@ -3,22 +3,24 @@ package com.calldatabase.jsp.servlet;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.lang.Object;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+
 public class CallDatabase extends HttpServlet {
-	static final long serialVersionUID = 1L;
-
-	
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
     	// Connect to database
         String url = "jdbc:sqlserver://softwareengineeringuc.database.windows.net:1433;database=SoftwareEngineeringUC;user=ufkesba@softwareengineeringuc;password=Scout!2063;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;";
         Connection connection = null;
@@ -30,33 +32,72 @@ public class CallDatabase extends HttpServlet {
                 connection = DriverManager.getConnection(url);
                 String schema = connection.getSchema();
 
-                // Create and execute a SELECT SQL statement.
-                String selectSql = "SELECT * FROM [User]";
+                String insertSql = "INSERT INTO Event (UserID, EventID, EventType, Title, Description, Location, DateTimeStart, DateTimeEnd) VALUES (?,?,?,?,?,?,?,?);";
+                
+                Date d = Date.valueOf("2017-28-06");
+				
+                try (PreparedStatement prep = connection.prepareStatement(insertSql)) {
+                        prep.setString(1, "0");
+                        prep.setString(2, "1");
+                        prep.setInt(3, 1);
+                        prep.setString(4, "");
+                        prep.setString(5, "");
+                        prep.setString(6, "");
+                        prep.setDate(7, d);
+                        
+                        int count = prep.executeUpdate();
+                        System.out.println("Inserted: " + count + " row(s)");
 
-                try (Statement statement = connection.createStatement();
-                        ResultSet resultSet = statement.executeQuery(selectSql)) {
-
-                            // Print results from select statement
-                            queryResult = "First two columns: ";
-                            int i = 0;
-                            while (resultSet.next())
-                            {
-                            	if (i > 0) {
-                            		queryResult += ", ";
-                            	}
-                                queryResult += resultSet.getString(1) + " "
-                                    + resultSet.getString(2);
-                                i++;
-                            }
-                    }
+                }
+//                // Create and execute a SELECT SQL statement.
+//                String selectSql = "SELECT * FROM [User]";
+//
+//                try (Statement statement = connection.createStatement();
+//                        ResultSet resultSet = statement.executeQuery(selectSql)) {
+//
+//                            // Print results from select statement
+//                            queryResult = "First two columns: ";
+//                            int i = 0;
+//                            while (resultSet.next())
+//                            {
+//                            	if (i > 0) {
+//                            		queryResult += ", ";
+//                            	}
+//                                queryResult += resultSet.getString(1) + " "
+//                                    + resultSet.getString(2);
+//                                i++;
+//                            }
+//                    }
         }
         catch (Exception e) {
                 e.printStackTrace();
         }
         
-    	response.setContentType("text/plain");
-    	response.setCharacterEncoding("UTF-8");
-    	response.getWriter().write(queryResult);
-             
-        }
-}
+           
+        
+    	finally
+    	{
+    		
+    		
+          //Print new HTML screen
+            PrintWriter out = response.getWriter();
+            out.println (
+                      "<!DOCTYPE html PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\" +" +
+                          "http://www.w3.org/TR/html4/loose.dtd\">\n" +
+                      "<html> \n" +
+                        "<head> \n" +
+                          "<meta http-equiv=\"Content-Type\" content=\"text/html; " +
+                            "charset=ISO-8859-1\"> \n" +
+                          "<title> CallDatabase </title> \n" +
+                        "</head> \n" +
+                        "<body> <div align='center'> \n" +
+                          "<style= \"font-size=\"12px\" color='black'\"" + "\">" +
+                            queryResult + " <br> " +
+                        "</font></body> \n" +
+                      "</html>" 
+                    );   
+    	}
+    }
+    	
+    }
+
