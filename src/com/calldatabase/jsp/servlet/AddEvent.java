50c212654cff32c2cf4579781fdb4880191f64e3
ufkesba <brentufkes@hotmail.com>
2017-07-08 20:12:52 -0400

    Change to PreparedStatement
    Fixed an error in the Tomcat server log

--- src/com/calldatabase/jsp/servlet/AddEvent.java
+++ src/com/calldatabase/jsp/servlet/AddEvent.java
@@ -7,6 +7,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.sql.Timestamp;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -25,14 +26,14 @@ public class AddEvent extends HttpServlet {
 	        String driver = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
 	        String queryResult = "test";
 	        
-	        String UserID = "'0'";
-	        String EventID = "'1'";
+	        String UserID = "test2";
+	        String EventID = "1";
 	        int EventType = 1;
-	        String Title = "'SampleEvent'";
-	        String Description = "'SampleDescription'";
-	        String Location = "'Campus'";
-	        String DateTimeStart = "'2017-07-28 13:30:00'";
-	        String DateTimeEnd = "'2017-07-28 14:30:00'";
+	        String Title = "SampleEvent";
+	        String Description = "SampleDescription";
+	        String Location = "Campus";
+	        Timestamp DateTimeStart = Timestamp.valueOf("2017-07-28 12:30:00");
+	        Timestamp DateTimeEnd = Timestamp.valueOf("2017-07-28 14:30:00");
 
 	        try {
 	        	    Class.forName(driver).newInstance();
@@ -41,21 +42,23 @@ public class AddEvent extends HttpServlet {
 
 	                // Prepared statement to insert data
 	                String insertSql = "INSERT INTO Event (UserID, EventID, EventType, Title, Description, Location, DateTimeStart, DateTimeEnd)"
-	                		+ " VALUES ("
-	                		+ UserID + ","
-	                		+ EventID + ","
-	                		+ EventType + ","
-	                		+ Title + ","
-	                		+ Description + ","
-	                		+ Location + ","
-	                		+ DateTimeStart + ","
-	                		+ DateTimeEnd + ")";
+	                		+ " VALUES (?,?,?,?,?,?,?,?)";
 	                System.out.println(insertSql);
 					
 	                //This will need to be changed to PreparedStatement
-	                try (Statement statement = connection.createStatement();
-	                        ResultSet resultSet = statement.executeQuery(insertSql)) {
+	                try (PreparedStatement statement = connection.prepareStatement(insertSql)) {
+	                	statement.setString(1, UserID);
+	                	statement.setString(2, EventID);
+	                	statement.setInt(3, EventType);
+	                	statement.setString(4, Title);
+	                	statement.setString(5, Description);
+	                	statement.setString(6, Location);
+	                	statement.setTimestamp(7, DateTimeStart);
+	                	statement.setTimestamp(8, DateTimeEnd);
 
+                        int count = statement.executeUpdate();
+                        System.out.println("Inserted: " + count + " row(s)");
+	                
 	                }
 	                queryResult = insertSql;
 	        }
