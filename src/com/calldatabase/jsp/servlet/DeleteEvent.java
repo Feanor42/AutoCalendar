2d24faebd3de86fa6cbc80e7b92729a1b6f6f6df
ufkesba <brentufkes@hotmail.com>
2017-07-12 21:42:08 -0400

    Working Delete Event

--- src/com/calldatabase/jsp/servlet/DeleteEvent.java
+++ src/com/calldatabase/jsp/servlet/DeleteEvent.java
@@ -5,62 +5,38 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.Statement;
 import java.sql.Timestamp;
+import java.util.Enumeration;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-
-//<Brent Ufkes> 09-July-2017
-//Delete event servlet
+// <Brent Ufkes> 09-July-2017
+// Delete event servlet
 public class DeleteEvent extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	 protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-		 	//initializing variables
-		 	int UserID = 0;
-	        String Title = "";
-	        String Description = "";
-	        String DateTimeStart = "";
-	        String DateTimeEnd = "";
-		 
-	        //building json object
+		 	// Recieve request, convert to string, save as variable
 		 	StringBuilder sb = new StringBuilder();
-		    BufferedReader br = request.getReader();
-		    String str;
-		    
-		    JSONObject jObj;
-		    
-		    
-		    //updating parameters
-		    while( (str = br.readLine()) != null ){
-		        sb.append(str);
-		    }    
-		    
+		    BufferedReader reader = request.getReader();
 		    try {
-				jObj = new JSONObject(sb.toString());
-				UserID = 0;
-				Title = jObj.getString("title");
-				Description = jObj.getString("description");
-				DateTimeStart = jObj.getString("start");
-				DateTimeEnd = jObj.getString("end");
-				
-			} catch (JSONException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-			}
-		 
+		        String line;
+		        while ((line = reader.readLine()) != null) {
+		            sb.append(line);
+		        }
+		    } finally {
+		        reader.close();
+		    }
+		    String EventIDString = sb.toString();
+		    int EventID = Integer.parseInt(EventIDString);
+		    
 	    	// Connect to database
 	        String url = "jdbc:sqlserver://softwareengineeringuc.database.windows.net:1433;database=SoftwareEngineeringUC;user=ufkesba@softwareengineeringuc;password=Scout!2063;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;";
 	        Connection connection = null;
 	        String driver = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
-	        String queryResult = "";
 	        
 
 	        try {
@@ -69,27 +45,17 @@ public class DeleteEvent extends HttpServlet {
 	                String schema = connection.getSchema();
 
 	                // Prepared statement to insert data
-	                String deleteSql = "Delete Event"
-	                		+ " WHERE DateTimeStart='" + DateTimeStart + "' AND DateTimeEnd='" + DateTimeEnd + "'";
-	                System.out.println(deleteSql);
+	                String insertSql = "Delete Event WHERE EventID=" + EventID;
 					
-	                try {
-	                	Statement statement = connection.createStatement();
-	                    ResultSet resultSet = statement.executeQuery(deleteSql);
-	                    //response.getWriter().write(convertToJSON(resultSet).toString());
-	                }
-	                catch (Exception e) {
-	                    e.printStackTrace();
+	                //This will need to be changed to PreparedStatement
+	                try (PreparedStatement statement = connection.prepareStatement(insertSql)) {
+                        int count = statement.executeUpdate();
 	                }
 	        }
 	        catch (Exception e) {
 	                e.printStackTrace();
 	        }
-	        
-//	    	response.setContentType("text/plain");
-//	    	response.setCharacterEncoding("UTF-8");
-//	    	response.getWriter().write(queryResult);
 	             
-	        }
+	  }
 	
 }
